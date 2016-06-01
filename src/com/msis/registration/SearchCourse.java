13df5c0044852511e32012c8a03caf4f3e8fa4d7
Nafiz - Al - Naharul Islam <nafiz351@yahoo.com>
2016-06-01 00:44:06 -0400

    Meeting Date, Status added

--- src/com/msis/registration/SearchCourse.java
+++ src/com/msis/registration/SearchCourse.java
@@ -39,24 +39,27 @@ public SearchCourse(){
 		try{			
 			// Establish Connection
 			String sqlQuery=null;
-			conn = obj.DBConnect();
-			
-			sqlQuery="SELECT ti.term as term,CONCAT(ins.first_name,' ',ins.last_name) as instructor,CONCAT(sch.day,' ',sch.start_time,' - ',sch.end_time) as time, CONCAT(bld.name,' ',rm.room_no,' ',bld.campus_short) as location, CONCAT(sbj.subject_code,' ',cs.course_code,' - ',cs.title) as course"; 
-			sqlQuery+=" FROM course_details cd, course cs, subject sbj, term_info ti, instructor ins, schedule sch, room rm, building bld"; 
-			sqlQuery+=" WHERE ti.id=? and cs.level=? AND";
-			sqlQuery+=" sbj.subject_code=? AND"; 
-			
+			conn = obj.DBConnect();			
+
+			sqlQuery="SELECT ti.term as term,CONCAT(ins.first_name,' ',ins.last_name) as instructor,GROUP_CONCAT(CONCAT(sch.day,' ',sch.start_time,' - ',sch.end_time) SEPARATOR ' & ') as time, CONCAT(bld.name,' ',rm.room_no,' ',bld.campus_short) as location, CONCAT(sbj.subject_code,' ',cs.course_code,' - ',cs.title) as course";
+			sqlQuery+=", if((cd.class_capacity -(select count(*) from registration rgs where cd.id=rgs.course_details_id))>0,'Open','Closed') as status, CONCAT(DATE_FORMAT(ti.start_date,'%d/%m/%Y'),' - ', DATE_FORMAT(DATE_ADD(ti.start_date, INTERVAL cd.duration week),'%d/%m/%Y')) as meeting";
+			sqlQuery+=" FROM course_details cd, course cs, subject sbj, term_info ti, instructor ins, schedule sch, course_schedule csch, room rm, building bld";
+			sqlQuery+=" WHERE ti.id=? and"; 
+			sqlQuery+=" cs.level=? AND";
+			sqlQuery+=" sbj.subject_code=? AND";
 			if(courseNo!=""){
 				sqlQuery+=" cs.course_code=? AND";
 			}
-			
-			sqlQuery+=" cd.term_id=ti.id and";
+			sqlQuery+=" cd.term_id=ti.id AND";
 			sqlQuery+=" cd.instructor_id=ins.id and"; 
-			sqlQuery+=" cd.schedule_id=sch.id AND"; 
 			sqlQuery+=" cd.room_id=rm.id AND"; 
 			sqlQuery+=" rm.building_id =bld.id AND"; 
 			sqlQuery+=" cd.course_id=cs.id AND"; 
-			sqlQuery+=" cs.subject_id=sbj.id";
+			sqlQuery+=" cs.subject_id=sbj.id and";
+			sqlQuery+=" csch.schedule_id=sch.id AND";
+			sqlQuery+=" csch.course_detail_id=cd.id";
+			sqlQuery+=" GROUP BY csch.course_detail_id";
+			
 			
 			System.out.print(sqlQuery+"\n");
 			//String sqlQuery="SELECT ti.term as term,CONCAT(ins.first_name,' ',ins.last_name) as instructor,CONCAT(sch.day,' ',sch.start_time,' - ',sch.end_time) as time, CONCAT(bld.name,' ',rm.room_no,' ',bld.campus_short) as location, CONCAT(sbj.subject_code,' ',cs.course_code,' - ',cs.title) as course FROM course_details cd, course cs, subject sbj, term_info ti, instructor ins, schedule sch, room rm, building bld WHERE ti.id=? and cs.level=? AND sbj.subject_code=? AND cd.term_id=ti.id and cd.instructor_id=ins.id and cd.schedule_id=sch.id AND cd.room_id=rm.id AND rm.building_id =bld.id AND cd.course_id=cs.id AND cs.subject_id=sbj.id";
@@ -73,8 +76,9 @@ public SearchCourse(){
 			}
 			
 			ResultSet result = course.executeQuery();
-			ArrayList Rows = new ArrayList();
+			ArrayList Rows = new ArrayList();			
 			while(result.next()){
+				//System.out.println(dbID + dbName + dbStatusClient);
 				ArrayList row = new ArrayList();
 			    for (int i = 1; i <= 1 ; i++){
 			    	row.add(result.getString("term"));
@@ -82,6 +86,8 @@ public SearchCourse(){
 			    	row.add(result.getString("time"));
 			    	row.add(result.getString("location"));
 			    	row.add(result.getString("course"));
+			    	row.add(result.getString("status"));
+			    	row.add(result.getString("meeting"));
 			    }
 			    Rows.add(row);
 			}
@@ -103,7 +109,7 @@ public SearchCourse(){
 
 	private void doPerform(HttpServletRequest request,HttpServletResponse response) 
 	{
+	  
 	}
 	
 }
-
