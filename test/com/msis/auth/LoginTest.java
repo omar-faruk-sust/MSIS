acc061a2281046c58bd665fecb33a0c2ae553aee
uashra <u_ashra@encs.concordia.ca>
2016-06-13 02:01:22 -0400

    added first basic test for login page status check

---
+++ test/com/msis/auth/LoginTest.java
@@ -0,0 +1,21 @@
+package com.msis.auth;
+
+import java.net.*;
+import java.io.IOException;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class LoginTest {
+
+	@Test
+	public void testLoginPageGETStatus() throws IOException {
+		URL url = new URL("http://localhost:8080/MSIS/");
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.connect();
+		System.out.print(conn.getResponseCode());
+		assertEquals(200, conn.getResponseCode());
+		conn.disconnect();
+	}
+}
