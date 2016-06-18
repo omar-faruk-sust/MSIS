d2a7f0d85ed95cbe6a2050f81f86b8df76ba7510
Nafiz - Al - Naharul Islam <nafiz351@yahoo.com>
2016-06-18 19:39:43 -0400

    Update DueAmountModel.java

--- src/com/msis/model/DueAmountModel.java
+++ src/com/msis/model/DueAmountModel.java
@@ -48,4 +48,37 @@ public class DueAmountModel {
 		
 		return dueAmount;
 	}
+	
+	public double DueAmount(int studentId) {
+		
+		String sql = "select id,student_id,term_id,SUM(due_amount) as total_amount from "+ tableName +" where student_id=?";
+		
+		// Establish Connection
+		MySQLAccess obj = new MySQLAccess();
+		Connection connection = obj.getConnection();
+		int id = 0;
+		int student_Id = 0;
+		int term_id = 0;
+		Double payableDueAmount = 0.0;
+		
+		try {
+			PreparedStatement prepareStm = connection.prepareStatement(sql);
+			prepareStm.setInt(1, studentId);
+			ResultSet results = prepareStm.executeQuery();
+			while(results.next()){
+				 id = results.getInt("id");
+				 student_Id = results.getInt("student_id");
+				 term_id = results.getInt("term_id");
+				 payableDueAmount = results.getDouble("total_amount");
+				System.out.println("Due amount is: "+payableDueAmount);
+			}
+			
+			//dueAmount = new PaymentDue(id, student_Id, term_id, payableDueAmount);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return payableDueAmount;
+	}
 }
